828b58bb8bde6270c9c507696cfcea7a6e1b5f5e
Nikos Anastopoulos <n.anastop@gmail.com>
2018-10-02 16:52:23 +0300

    Include domain xml in creation script

--- dpdk-vm/make_vm.sh
+++ dpdk-vm/make_vm.sh
@@ -10,9 +10,9 @@ USER_NAME="ubuntu"
 USER_PASSWD="ubuntu"
 IMG="xenial"
 ARCH="amd64"
-IMG_URL="https://cloud-images.ubuntu.com/releases/16.04/release"
-IMG_NAME="ubuntu-16.04-server-cloudimg-${ARCH}-disk1.img"
-IMG_PATH=.
+CLOUD_IMG_URL="https://cloud-images.ubuntu.com/releases/16.04/release"
+CLOUD_IMG_NAME="ubuntu-16.04-server-cloudimg-${ARCH}-disk1.img"
+CLOUD_IMG_PATH=.
 KVM_DEF_POOL=default
 KVM_DEF_POOL_PATH=/var/lib/libvirt/images
 
@@ -21,7 +21,8 @@ GUEST_NAME="dpdk-vm"
 GUEST_DOMAIN=intranet.local
 #GUEST_VROOTDISKSIZE=10G
 GUEST_VCPUS=4
-GUEST_VMEM=4096
+let "LASTCPU=GUEST_VCPUS-1"
+GUEST_MEM_MB=4096
 GUEST_NETWORK="bridge=virbr0,model=virtio"
 
 
@@ -97,6 +98,90 @@ power_state:
   mode: reboot
 EOF
 
+cat <<EOF > dom.xml
+<domain type='kvm'>
+  <name>$GUEST_NAME</name>
+  <!-- uuid>4a9b3f53-fa2a-47f3-a757-dd87720d9d1d</uuid -->
+  <memory unit='MiB'>$GUEST_MEM_MB</memory>
+  <currentMemory unit='MiB'>$GUEST_MEM_MB</currentMemory>
+  <memoryBacking>
+    <hugepages/>
+    <!-- hugepages>
+      <page size='2' unit='M' nodeset='0'/>
+    </hugepages -->
+  </memoryBacking>
+  <vcpu placement='static'>$GUEST_VCPUS</vcpu>
+  <!-- cputune>
+    <shares>4096</shares>
+    <vcpupin vcpu='0' cpuset='4'/>
+    <vcpupin vcpu='1' cpuset='5'/>
+    <emulatorpin cpuset='4,5'/>
+  </cputune -->
+  <os>
+    <type arch='x86_64' machine='pc'>hvm</type>
+    <boot dev='hd'/>
+  </os>
+  <features>
+    <acpi/>
+    <apic/>
+  </features>
+  <cpu mode='host-model'>
+    <model fallback='allow'/>
+    <topology sockets='1' cores='$GUEST_VCPUS' threads='1'/>
+    <numa>
+      <cell id='0' cpus='0-$LASTCPU' memory='$GUEST_MEM_MB' unit='MiB' memAccess='shared'/>
+    </numa>
+  </cpu>
+  <on_poweroff>destroy</on_poweroff>
+  <on_reboot>restart</on_reboot>
+  <on_crash>destroy</on_crash>
+  <devices>
+    <emulator>/usr/bin/qemu-system-x86_64</emulator>
+    <!-- emulator>/usr/bin/kvm-spice</emulator -->
+    <disk type='file' device='disk'>
+      <driver name='qemu' type='qcow2'/>
+      <source file='${POOL_PATH}/${GUEST_NAME}.root.img'/>
+      <target dev='vda' bus='virtio'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x07' function='0x0'/>
+    </disk>
+    <disk type='file' device='disk'>
+      <driver name='qemu' type='raw'/>
+      <source file='${POOL_PATH}/${GUEST_NAME}.configuration.iso'/>
+      <target dev='vdb' bus='virtio'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x08' function='0x0'/>
+    </disk>
+    <interface type='bridge'>
+      <!-- mac address='52:54:00:ec:96:13'/ -->
+      <source bridge='virbr0'/>
+      <model type='virtio'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
+    </interface>
+    <!-- interface type='vhostuser'>
+      <mac address='00:00:00:00:00:01'/>
+      <source type='unix' path='/usr/local/var/run/openvswitch/dpdkvhostuser0' mode='client'/>
+      <model type='virtio'/>
+      <driver queues='2'>
+        <host mrg_rxbuf='on'/>
+      </driver>
+    </interface>
+    <interface type='vhostuser'>
+      <mac address='00:00:00:00:00:02'/>
+      <source type='unix' path='/usr/local/var/run/openvswitch/dpdkvhostuser1' mode='client'/>
+      <model type='virtio'/>
+      <driver queues='2'>
+        <host mrg_rxbuf='on'/>
+      </driver>
+    </interface-->
+    <serial type='pty'>
+      <target port='0'/>
+    </serial>
+    <console type='pty'>
+      <target type='serial' port='0'/>
+    </console>
+  </devices>
+</domain>
+EOF
+
 # don't edit below unless you know wat you're doing!
 
 # check if the script is run as root user
@@ -105,9 +190,9 @@ if [[ $USER != "root" ]]; then
 fi
 
 
-if [[ ! -f ${IMG_PATH}/${IMG_NAME} ]]; then
-  echo "Downloading image ${IMG_NAME}..."
-  wget ${IMG_URL}/${IMG_NAME} -O ${IMG_PATH}/${IMG_NAME}
+if [[ ! -f ${CLOUD_IMG_PATH}/${CLOUD_IMG_NAME} ]]; then
+  echo "Downloading image ${CLOUD_IMG_NAME}..."
+  wget ${CLOUD_IMG_URL}/${CLOUD_IMG_NAME} -O ${CLOUD_IMG_PATH}/${CLOUD_IMG_NAME}
 fi
 
 # check if pool exists, otherwise create it
@@ -131,13 +216,13 @@ cp configuration.iso ${POOL_PATH}/${GUEST_NAME}.configuration.iso
 virsh pool-refresh ${POOL}
 
 # copy image to libvirt's pool
-if [[ ! -f ${POOL_PATH}/${IMG_NAME} ]]; then
-  cp ${IMG_PATH}/${IMG_NAME} ${POOL_PATH}
+if [[ ! -f ${POOL_PATH}/${CLOUD_IMG_NAME} ]]; then
+  cp ${CLOUD_IMG_PATH}/${CLOUD_IMG_NAME} ${POOL_PATH}
   virsh pool-refresh ${POOL}
 fi
 
 # clone cloud image
-virsh vol-clone --pool ${POOL} ${IMG_NAME} ${GUEST_NAME}.root.img
+virsh vol-clone --pool ${POOL} ${CLOUD_IMG_NAME} ${GUEST_NAME}.root.img
 # virsh vol-resize --pool ${POOL} ${GUEST_NAME}.root.img ${GUEST_VROOTDISKSIZE}
 
 # convert image format
@@ -151,7 +236,7 @@ fi
 echo "Creating guest ${GUEST_NAME}..."
 #virt-install \
 #  --name ${GUEST_NAME} \
-#  --ram ${GUEST_VMEM} \
+#  --ram ${GUEST_MEM_MB} \
 #  --vcpus=${GUEST_VCPUS},sockets=1,cores=${GUEST_VCPUS},threads=1 \
 #  --autostart \
 #  --memballoon virtio \
@@ -160,7 +245,8 @@ echo "Creating guest ${GUEST_NAME}..."
 #  --disk vol=${POOL}/${GUEST_NAME}.root.img,format=${FORMAT},bus=virtio \
 #  --disk vol=${POOL}/${GUEST_NAME}.configuration.iso,bus=virtio \
 #  --noautoconsole
-virsh create sample.xml
+virsh define dom.xml
+virsh start ${GUEST_NAME}
 
 # display result
 echo
