a6434519d7216ba52387e521656495d867e5f94a
Nikos Anastopoulos <n.anastop@gmail.com>
2018-10-03 10:47:53 +0300

    Final updates

--- dpdk-vm/README.md
+++ dpdk-vm/README.md
@@ -1,5 +1,32 @@
+This script creates a VM provisioned with: 
+- Ubuntu 16.04
+- DPDK built under /opt/nfv
+- user-defined username and password 
+
+Also, it launches a guest VM with the following capabilities: 
+- configurable #vCPUs (all in a single socket)
+- configurable memory size
+- hugepages (assumes host support)
+- vhostuser-type network interfaces to use with OVS-DPDK
+- bridge-type network interfaces for management 
+
+The script searches for vars `GUEST_NETIF{X}_TYPE` to create the 
+corresponding network interfaces, where X=1..10. 
+
+The following parameters can be specified for vhostuser-type networks interfaces: 
+- MAC address
+- Unix path
+- number of queues
+
+and for bridge-type interfaces: 
+- host bridge (e.g. `virbr0`)
+
 ## Usage 
 
-```
-sudo bash make_vm.sh
-```
+To provision VM image: 
+- edit `vars.sh`
+- `sudo bash create.sh provision`
+
+To launch VM instance: 
+- edit `vars.sh` as many times as needed
+- `sudo bash create.sh lauch`
---
+++ dpdk-vm/create.sh
@@ -0,0 +1,246 @@
+#!/usr/bin/env bash
+
+source vars.sh
+set -e
+# requires the following packages on Ubuntu host:
+#  apt-get install qemu-kvm libvirt-bin virtinst bridge-utils cloud-image-utils
+
+let "LASTCPU=GUEST_VCPUS-1"
+
+show_help() {
+	echo "Usage:"
+	echo "1. Edit vars.sh according to the template"
+	echo "2. Run 'sudo bash create.sh provision' to provision the VM image"
+	echo "3. Run 'sudo bash create.sh launch' to launch the instance"
+}
+
+create_cloud_config() {
+	# cloud-init config files : specify cloud-init data for your guest
+	cat <<EOF > meta-data
+instance-id: iid-${GUEST_NAME};
+#hostname: ${GUEST_NAME}
+#local-hostname: ${GUEST_NAME}
+EOF
+
+	cat <<EOF > user-data
+#cloud-config
+users:
+  - name: ${USER_NAME}
+    lock_passwd: false
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    shell: /bin/bash
+    passwd: $(openssl passwd -1 -salt SaltSalt ${USER_PASSWD})
+write_files:
+  - path: /provision.sh
+    content: |
+      #!/usr/bin/env bash
+      mkdir -p /opt/nfv 
+      cd /opt/nfv
+      wget http://fast.dpdk.org/rel/dpdk-16.07.tar.xz
+      tar -xvf dpdk-16.07.tar.xz
+      export RTE_SDK=$(pwd)/dpdk-16.07
+      export RTE_TARGET=x86_64-native-linuxapp-gcc
+      cd $RTE_SDK
+      make config T=x86_64-native-linuxapp-gcc
+      make install T=x86_64-native-linuxapp-gcc DESTDIR=dpdk-install
+
+  - path: /remove_cloud_init.sh
+    content: |
+      #!/usr/bin/env bash
+      echo 'datasource_list: [ None ]' | sudo -s tee /etc/cloud/cloud.cfg.d/90_dpkg.cfg
+      sudo apt-get purge -y cloud-init
+      sudo rm -rf /etc/cloud/; sudo rm -rf /var/lib/cloud/
+
+
+password: $USER_PASSWD
+chpasswd: { expire: False }
+ssh_pwauth: True
+
+# upgrade packages on startup
+package_upgrade: true
+
+#run 'apt-get upgrade' or yum equivalent on first boot
+apt_upgrade: true
+
+packages: 
+  - gcc
+  - make
+
+runcmd:
+  - bash /provision.sh
+  - bash /remove_cloud_init.sh
+  - shutdown
+
+power_state:
+  mode: reboot
+EOF
+}
+
+create_dom_xml() {
+	cat <<EOF > dom.xml
+<domain type='kvm'>
+  <name>$GUEST_NAME</name>
+  <!-- uuid>4a9b3f53-fa2a-47f3-a757-dd87720d9d1d</uuid -->
+  <memory unit='MiB'>$GUEST_MEM_MB</memory>
+  <currentMemory unit='MiB'>$GUEST_MEM_MB</currentMemory>
+  <memoryBacking>
+    <hugepages/>
+    <!-- hugepages>
+      <page size='2' unit='M' nodeset='0'/>
+    </hugepages -->
+  </memoryBacking>
+  <vcpu placement='static'>$GUEST_VCPUS</vcpu>
+  <!-- cputune>
+    <shares>4096</shares>
+    <vcpupin vcpu='0' cpuset='4'/>
+    <vcpupin vcpu='1' cpuset='5'/>
+    <emulatorpin cpuset='4,5'/>
+  </cputune -->
+  <os>
+    <type arch='x86_64' machine='pc'>hvm</type>
+    <boot dev='hd'/>
+  </os>
+  <features>
+    <acpi/>
+    <apic/>
+  </features>
+  <cpu mode='host-model'>
+    <model fallback='allow'/>
+    <topology sockets='1' cores='$GUEST_VCPUS' threads='1'/>
+    <numa>
+      <cell id='0' cpus='0-$LASTCPU' memory='$GUEST_MEM_MB' unit='MiB' memAccess='shared'/>
+    </numa>
+  </cpu>
+  <on_poweroff>destroy</on_poweroff>
+  <on_reboot>restart</on_reboot>
+  <on_crash>destroy</on_crash>
+  <devices>
+    <emulator>/usr/bin/qemu-system-x86_64</emulator>
+    <!-- emulator>/usr/bin/kvm-spice</emulator -->
+    <disk type='file' device='disk'>
+      <driver name='qemu' type='qcow2'/>
+      <source file='${POOL_PATH}/${GUEST_NAME}.root.img'/>
+      <target dev='vda' bus='virtio'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x07' function='0x0'/>
+    </disk>
+    <disk type='file' device='disk'>
+      <driver name='qemu' type='raw'/>
+      <source file='${POOL_PATH}/${GUEST_NAME}.configuration.iso'/>
+      <target dev='vdb' bus='virtio'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x08' function='0x0'/>
+    </disk>
+EOF
+
+	for NIF in `seq 1 10`
+	do
+		t="GUEST_NETIF${NIF}_TYPE"
+	  	if [[ -v t ]]
+  		then
+			if [ "${!t}" = "bridge" ]
+			then
+				b="GUEST_NETIF${NIF}_BRIDGE"
+				echo "    <interface type='${!t}'>" >> dom.xml
+				echo "      <source bridge='${!b}' />" >> dom.xml
+				echo "      <model type='virtio'/>" >> dom.xml
+				echo "    </interface>" >> dom.xml
+
+			elif [ "${!t}" = "vhostuser" ]
+			then
+				m="GUEST_NETIF${NIF}_MACADDR"
+				u="GUEST_NETIF${NIF}_UNIXPATH"
+				q="GUEST_NETIF${NIF}_QUEUES"
+				echo "    <interface type='${!t}'>" >> dom.xml
+				echo "      <mac address='${!m}'/>" >> dom.xml
+      			        echo "      <source type='unix' path='${!u}' mode='client'/>" >> dom.xml
+      		        	echo "      <model type='virtio'/>" >> dom.xml
+	      		        echo "      <driver queues='${!q}'>" >> dom.xml
+        		        echo "        <host mrg_rxbuf='on'/>" >> dom.xml
+      			        echo "      </driver>" >> dom.xml
+				echo "    </interface>" >> dom.xml
+		        fi
+	  	fi
+	done
+
+	cat <<EOF >> dom.xml    
+    <serial type='pty'>
+      <target port='0'/>
+    </serial>
+    <console type='pty'>
+      <target type='serial' port='0'/>
+    </console>
+  </devices>
+</domain>
+EOF
+}
+
+download_image() {
+	if [[ ! -f ${CLOUD_IMG_PATH}/${CLOUD_IMG_NAME} ]]; then
+		echo "Downloading image ${CLOUD_IMG_NAME}..."
+      		wget ${CLOUD_IMG_URL}/${CLOUD_IMG_NAME} -O ${CLOUD_IMG_PATH}/${CLOUD_IMG_NAME}
+	fi
+}
+
+create_volumes() {
+	# check if pool exists, otherwise create it
+	if [[ "$(virsh pool-list|grep ${POOL} -c)" -ne "1" ]]; then
+		virsh pool-define-as --name ${POOL} --type dir --target ${POOL_PATH}
+	      	virsh pool-autostart ${POOL}
+	      	virsh pool-build ${POOL}
+	      	virsh pool-start ${POOL}
+	fi
+
+	# write the two cloud-init files into an ISO
+	genisoimage -output configuration.iso -volid cidata -joliet -rock user-data meta-data
+	#cloud-localds configuration.iso user-data meta-data
+
+	# copy ISO into libvirt's directory
+	cp configuration.iso ${POOL_PATH}/${GUEST_NAME}.configuration.iso
+	virsh pool-refresh ${POOL}
+
+	# copy image to libvirt's pool
+	if [[ ! -f ${POOL_PATH}/${CLOUD_IMG_NAME} ]]; then
+	      cp ${CLOUD_IMG_PATH}/${CLOUD_IMG_NAME} ${POOL_PATH}
+      	      virsh pool-refresh ${POOL}
+      	fi
+
+	# clone cloud image
+	virsh vol-clone --pool ${POOL} ${CLOUD_IMG_NAME} ${GUEST_NAME}.root.img
+	# virsh vol-resize --pool ${POOL} ${GUEST_NAME}.root.img ${GUEST_VROOTDISKSIZE}
+
+	# convert image format
+	if [[ "${CONVERT}" == "yes" ]]; then
+		echo "Converting image to format ${FORMAT}..."
+	      	qemu-img convert -O ${FORMAT} ${POOL_PATH}/${GUEST_NAME}.root.img ${POOL_PATH}/${GUEST_NAME}.root.img.${FORMAT}
+	      	rm ${POOL_PATH}/${GUEST_NAME}.root.img
+	      	mv ${POOL_PATH}/${GUEST_NAME}.root.img.${FORMAT} ${POOL_PATH}/${GUEST_NAME}.root.img
+	fi
+}
+
+create_guest() {
+	virsh undefine ${GUEST_NAME} || true
+	virsh define dom.xml
+	virsh start ${GUEST_NAME}
+}
+
+display_result() {
+	virsh list
+}
+
+cleanup() {
+	rm configuration.iso user-data meta-data
+}
+
+if [ "$1" == "provision" ]; then
+	create_cloud_config
+	download_image
+	create_volumes
+	create_dom_xml
+	create_guest
+	display_result
+	cleanup
+elif [ "$1" == "launch" ]; then
+	create_dom_xml
+	create_guest
+else
+	show_help
+fi
