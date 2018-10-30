92cc0ef7b6f904e1662a151ea22bee7b3b62eaac
builder <builder@sys4.devcloud.lab>
2018-10-30 01:10:12 -0700

    Enhance deployment script

--- scenarios/multiple_streamers_cache_sensitive_collocation.sh
+++ scenarios/multiple_streamers_cache_sensitive_collocation.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 num_streamers=6
+first_streamer=1
 num_cache_sens=3
+first_cache_sens=1
 
-for id in `seq 1 1 $num_cache_sens`; do
-	let "port = 8801 + id"
+let "last_streamer = first_streamer + num_streamers"
+let "last_cache_sens = first_cache_sens + num_cache_sens"
+
+for id in {$first_cache_sens..$last_cache_sens}; do
+	let "port = 8800 + id"
 	docker run --rm --detach -p $port:8001 --name cache-sensitive${id} nfvsap/cache-sensitive
 done
 
-for id in `seq 1 1 $num_streamers`; do
-	let "port = 8902 + id"
+for id in {$first_streamer..$last_streamer}; do
+	let "port = 9800 + id"
 	docker run --rm --detach -p $port:8002 --name streamer${id} nfvsap/streamer
 done
 
