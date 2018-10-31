ec8a491aefc179923e49cd385838a1f5b4f65594
Nikos Anastopoulos <n.anastop@gmail.com>
2018-10-31 05:34:23 -0700

    Fix script

--- scenarios/multiple_streamers_cache_sensitive_collocation.sh
+++ scenarios/multiple_streamers_cache_sensitive_collocation.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-num_streamers=6
-first_streamer=1
-num_cache_sens=3
-first_cache_sens=1
+first_streamer_id=10
+last_streamer_id=11
+first_cache_sens_id=10
+last_cache_sens_id=10
 
-let "last_streamer = first_streamer + num_streamers"
-let "last_cache_sens = first_cache_sens + num_cache_sens"
-
-for id in {$first_cache_sens..$last_cache_sens}; do
+for (( id=$first_cache_sens_id; id<=$last_cache_sens_id; id++ )); do
 	let "port = 8800 + id"
 	docker run --rm --detach -p $port:8001 --name cache-sensitive${id} nfvsap/cache-sensitive
 done
 
-for id in {$first_streamer..$last_streamer}; do
+for (( id=$first_streamer_id; id<=$last_streamer_id; id++ )); do
 	let "port = 9800 + id"
 	docker run --rm --detach -p $port:8002 --name streamer${id} nfvsap/streamer
 done
