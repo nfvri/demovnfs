89463dfbebdabc161187cf0f31c2ec05885231e6
Nikos Anastopoulos <n.anastop@gmail.com>
2019-05-16 15:55:03 +0300

    Increase streamer intensity

--- streamer/virgo_initd.sh
+++ streamer/virgo_initd.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-/vnf/archbench/memory_tests/seqacc 80 &
-/vnf/archbench/memory_tests/seqacc 80 &
-/vnf/archbench/memory_tests/seqacc 80 &
-/vnf/archbench/memory_tests/seqacc 80 2>&1 >/dev/null | /vnf/simple-em/simple-em
+/vnf/archbench/memory_tests/seqacc 150 &
+/vnf/archbench/memory_tests/seqacc 150 &
+/vnf/archbench/memory_tests/seqacc 150 &
+/vnf/archbench/memory_tests/seqacc 150 &
+/vnf/archbench/memory_tests/seqacc 150 &
+/vnf/archbench/memory_tests/seqacc 150 &
+/vnf/archbench/memory_tests/seqacc 150 &
+/vnf/archbench/memory_tests/seqacc 150 2>&1 >/dev/null | /vnf/simple-em/simple-em
