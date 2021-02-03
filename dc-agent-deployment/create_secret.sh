3540cb992703658a2d97f072278fc432d6327e83
Evangelos Angelou <angelouev@intracom-telecom.com>
2021-02-03 12:48:15 +0200

    Update dc-agent daemonset deployment

--- dc-agent-deployment/create_secret.sh
+++ dc-agent-deployment/create_secret.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SECRETNAME=nfvsap-private-repo
+SECRETNAME=nfvri-private-repo
 kubectl create secret docker-registry $SECRETNAME --docker-server=https://gcr.io --docker-username=_json_key --docker-email=user@example.com --docker-password="$(cat k8s-gcr-auth-ro.json)"
 kubectl -n kube-system create secret docker-registry $SECRETNAME --docker-server=https://gcr.io --docker-username=_json_key --docker-email=user@example.com --docker-password="$(cat k8s-gcr-auth-ro.json)"
 
