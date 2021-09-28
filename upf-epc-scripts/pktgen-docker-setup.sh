48e6255cf9cb06980a5cfdf7a29a3fc63faa2b0f
gpanouts <gpanoutsak@gmail.com>
2021-09-28 12:07:27 +0300

    Change pktgens configurations and add another pktgen

--- upf-epc-scripts/pktgen-docker-setup.sh
+++ upf-epc-scripts/pktgen-docker-setup.sh
@@ -25,7 +25,13 @@ docker run --name pktgen2 -td --restart unless-stopped \
 docker run --name pktgen3 -td --restart unless-stopped \
         --cpuset-cpus=10,12 --ulimit memlock=-1 --cap-add IPC_LOCK \
         -v /dev/hugepages:/dev/hugepages -v "$PWD/conf":/opt/bess/bessctl/conf  \
-        --device=/dev/vfio/vfio --device=/dev/vfio/147 \
+        --device=/dev/vfio/vfio --device=/dev/vfio/153 \
+        omecproject/upf-epc-bess:master-latest -grpc-url=0.0.0.0:10514
+
+docker run --name pktgen4 -td --restart unless-stopped \
+        --cpuset-cpus=14,16 --ulimit memlock=-1 --cap-add IPC_LOCK \
+        -v /dev/hugepages:/dev/hugepages -v "$PWD/conf":/opt/bess/bessctl/conf  \
+        --device=/dev/vfio/vfio --device=/dev/vfio/154 \
         omecproject/upf-epc-bess:master-latest -grpc-url=0.0.0.0:10514
 
 ## Wait and start pktgen proper
@@ -35,6 +41,8 @@ sleep 20
 docker exec -it pktgen2 ./bessctl run pktgen2
 sleep 20
 docker exec -it pktgen3 ./bessctl run pktgen3
+sleep 20
+docker exec -it pktgen4 ./bessctl run pktgen4
 
 
 
